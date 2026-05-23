7eb4b4812ad0a8ffbe064bd9951501041369771e
ghkdqhrbals <ghkdqhrbals@naver.com>
2026-05-24 03:13:44 +0900

    Fix pkg version and install location

--- packaging/build-notarized-pkg.sh
+++ packaging/build-notarized-pkg.sh
@@ -7,7 +7,8 @@ DIST_DIR="$ROOT_DIR/dist"
 PKG_ROOT="$DIST_DIR/pkg-root"
 APP_PATH="$DERIVED_DATA/Build/Products/Release/LidStay.app"
 PKG_PATH="$DIST_DIR/LidStay.pkg"
-VERSION="${LIDSTAY_VERSION:-1.0}"
+COMPONENT_PLIST="$DIST_DIR/pkg-components.plist"
+VERSION="${LIDSTAY_VERSION:-}"
 SPARKLE_FEED_URL="${SPARKLE_FEED_URL:-https://github.com/ghkdqhrbals/LidStay/releases/latest/download/appcast.xml}"
 SPARKLE_PUBLIC_ED_KEY="${SPARKLE_PUBLIC_ED_KEY:-SFBDrWBr+kRxZOiUM1xHY+XgbC8vqAZ9fcLadJ9Trmw=}"
 DEVELOPER_ID_APPLICATION="${DEVELOPER_ID_APPLICATION:-Developer ID Application: gyumin hwangbo (4CL25TC734)}"
@@ -49,16 +50,24 @@ codesign \
 codesign --verify --deep --strict --verbose=2 "$APP_PATH"
 spctl --assess --type execute --verbose "$APP_PATH" || true
 
-rm -rf "$PKG_ROOT" "$PKG_PATH"
+if [[ -z "$VERSION" ]]; then
+  VERSION="$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$APP_PATH/Contents/Info.plist")"
+fi
+VERSION="${VERSION#v}"
+
+rm -rf "$PKG_ROOT" "$PKG_PATH" "$COMPONENT_PLIST"
 mkdir -p "$PKG_ROOT/Applications" "$PKG_ROOT/usr/local/bin"
 
 COPYFILE_DISABLE=1 ditto --norsrc --noextattr --noqtn --noacl "$APP_PATH" "$PKG_ROOT/Applications/LidStay.app"
 install -m 755 "$ROOT_DIR/CLI/lidstay" "$PKG_ROOT/usr/local/bin/lidstay"
 find "$PKG_ROOT" -name '._*' -delete
 xattr -cr "$PKG_ROOT"
+pkgbuild --analyze --root "$PKG_ROOT" "$COMPONENT_PLIST"
+/usr/libexec/PlistBuddy -c "Set :0:BundleIsRelocatable false" "$COMPONENT_PLIST"
 
 COPYFILE_DISABLE=1 pkgbuild \
   --root "$PKG_ROOT" \
+  --component-plist "$COMPONENT_PLIST" \
   --identifier "com.ghkdqhrbals.LidStay.pkg" \
   --version "$VERSION" \
   --install-location / \
